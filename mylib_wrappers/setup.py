589c7afe21d574d7370ca5939de4a342f4fb10d0
Artur <artur300998@gmail.com>
2024-01-31 21:55:37 +0100

    Setup script tweaks and readme

--- mylib_wrappers/setup.py
+++ mylib_wrappers/setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup, Extension # deprecated, use setuptools?
+from distutils.core import setup, Extension
 
 extension_mod = Extension(
   name="pymylib",
   sources=["./cpp/mylib_wrap.cpp"],
   include_dirs=["../mylib/inc"],
-  libraries=["mylib"])
+  libraries=["mylib"],
+  library_dirs=["../mylib/build"])
 
 setup(name = "pymylib", ext_modules=[extension_mod])
